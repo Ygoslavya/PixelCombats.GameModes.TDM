e59ac16b8b65318465fc65782cd38f51377bac0a
Ygoslavya <michaelnovikovyg95@gmail.com>
2024-12-08 01:15:46 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -4,13 +4,13 @@ import * as teams from './default_teams.js';
 import * as default_timer from './default_timer.js';
 
 // настройки
-const WaitingPlayersTime = 10;
-const BuildBaseTime = 30;
-const KnivesModeTime = 40;
+const WaitingPlayersTime = 0;
+const BuildBaseTime = 1;
+const KnivesModeTime = 0;
 const GameModeTime = default_timer.game_mode_length_seconds();
-const MockModeTime = 20;
-const EndOfMatchTime = 8;
-const VoteTime = 20;
+const MockModeTime = 1;
+const EndOfMatchTime = 0;
+const VoteTime = 0;
 
 const KILL_SCORES = 5;
 const WINNER_SCORES = 10;
