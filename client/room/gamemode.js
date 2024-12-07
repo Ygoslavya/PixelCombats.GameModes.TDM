c37e75fd62c57baf8a8498c87ba567b275cd3938
Ygoslavya <michaelnovikovyg95@gmail.com>
2024-12-08 00:56:30 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -72,29 +72,8 @@ function SetEndOfMatch() {
     // Завершение игры и отображение результатов
     Game.GameOver(LeaderBoard.GetTeams());
 
-    // Сравнение результатов игроков после окончания игры
-    ComparePlayerScores();
-
     // Перезапуск игры через 3 секунды после окончания матча
     mainTimer.Restart(3); 
-}
-
-// Функция для сравнения очков игроков
-function ComparePlayerScores() {
-    let highestScorePlayer = null;
-    let highestScore = -Infinity;
-
-    for (const player of Players.All) {
-        const score = player.Properties.Scores.Value;
-        if (score > highestScore) {
-            highestScore = score;
-            highestScorePlayer = player;
-        }
-    }
-
-    if (highestScorePlayer) {
-        Ui.GetContext().Hint.Value += ` Highest Score: ${highestScorePlayer.Name} with ${highestScore} points!`;
-    }
 }
 
 // Таймер для перезапуска игры после окончания матча
@@ -113,7 +92,9 @@ function ResetGame() {
     for (const player of Players.All) {
         player.Properties.Scores.Value = SCORES_INITIAL_VALUE;
         player.Properties.Kills.Value = KILLS_INITIAL_VALUE;
-        player.Spawns.Remove(); // Удалить игрока перед новым спавном (если необходимо)
+        
+        // Удаляем игрока перед новым спавном (если необходимо)
+        player.Spawns.Remove(); 
         player.Spawns.Spawn(); // Спавн игрока для нового раунда
     }
 }
