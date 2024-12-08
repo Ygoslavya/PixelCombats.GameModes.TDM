bd638fc5e1a5a0df7306a6b9ba39110e5c4e8d12
Ygoslavya <michaelnovikovyg95@gmail.com>
2024-12-08 23:24:06 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -7,15 +7,15 @@ import * as default_timer from './default_timer.js';
 const WaitingPlayersTime = 1;
 const BuildBaseTime = 1;
 const KnivesModeTime = 1;
-const GameModeTime = 1;
-const MockModeTime = 1;
-const EndOfMatchTime = 1;
+const GameModeTime = 1; // Установлено время игры на 1 секунду
+const MockModeTime = 20;
+const EndOfMatchTime = 8;
 const VoteTime = 1;
 
 const KILL_SCORES = 5;
 const WINNER_SCORES = 10;
 const TIMER_SCORES = 5;
-const SCORES_TIMER_INTERVAL = 1;
+const SCORES_TIMER_INTERVAL = 30;
 
 // имена используемых объектов
 const WaitingStateValue = "Waiting";
@@ -144,7 +144,7 @@ Damage.OnKill.Add(function (player, killed) {
 scores_timer.OnTimer.Add(function () {
     for (const player of Players.All) {
         if (player.Team == null) continue; // если вне команд то не начисляем ничего по таймеру
-        player.Properties.Scores.Value += TIMER_SCORES;
+        player.Properties.Scores.Value += TIMER_SCORES; // Это можно оставить или убрать в зависимости от ваших требований.
     }
 });
 
@@ -182,114 +182,109 @@ function SetWaitingMode() {
     Spawns.GetContext().enable = false;
     mainTimer.Restart(WaitingPlayersTime);
 }
+
 function SetBuildMode() {
     stateProp.Value = BuildModeStateValue;
     Ui.GetContext().Hint.Value = "Hint/BuildBase";
-    var inventory = Inventory.GetContext();
-    inventory.Main.Value = false;
-    inventory.Secondary.Value = false;
-    inventory.Melee.Value = true;
-    inventory.Explosive.Value = false;
-    inventory.Build.Value = true;
+    
+	var inventory= Inventory.GetContext(); 
+	inventory.Main.Value= false; 
+	inventory.Secondary.Value= false; 
+	inventory.Melee.Value= true; 
+	inventory.Explosive.Value= false; 
+	inventory.Build.Value= true; 
 
-    // запрет нанесения урона
-    Damage.GetContext().DamageOut.Value = false;
+	// запрет нанесения урона
+	Damage.GetContext().DamageOut.Value= false;
 
-    mainTimer.Restart(BuildBaseTime);
-    Spawns.GetContext().enable = true;
-    SpawnTeams();
+	mainTimer.Restart(BuildBaseTime);
+	Spawns.GetContext().enable= true; 
+	SpawnTeams(); 
 }
+
 function SetKnivesMode() {
-    stateProp.Value = KnivesModeStateValue;
-    Ui.GetContext().Hint.Value = "Hint/KnivesMode";
-    
-    var inventory = Inventory.GetContext();
-    inventory.Main.Value = false;
-    inventory.Secondary.Value = false;
-    inventory.Melee.Value = true;
-    inventory.Explosive.Value = false;
-    inventory.Build.Value = true;
+	stateProp.Value= KnivesModeStateValue; 
+	Ui.GetContext().Hint.Value= "Hint/KnivesMode"; 
+	
+	var inventory= Inventory.GetContext(); 
+	inventory.Main.value= false; 
+	inventory.Secondary.value= false; 
+	inventory.Melee.value= true; 
+	inventory.Explosive.value= false; 
+	inventory.Build.value= true;
 
-    // разрешение нанесения урона
-    Damage.GetContext().DamageOut.Value = true;
+	// разрешение нанесения урона
+	Damage.GetContext().DamageOut.value= true;
 
-    mainTimer.Restart(KnivesModeTime);
-    Spawns.GetContext().enable = true;
-    SpawnTeams();
+	mainTimer.Restart(KnivesModeTime); 
+	Spawns.GetContext().enable= true; 
+	SpawnTeams(); 
 }
+
 function SetGameMode() {
-    
 	// разрешаем нанесение урона
-	Damage.GetContext().DamageOut.Value= true; 
-	stateProp.Value= GameStateValue; 
-	Ui.GetContext().Hint.Value= "Hint/AttackEnemies"; 
+	Damage.GetContext().DamageOut.value= true; 
+	stateProp.value= GameStateValue; 
+	Ui.GetContext().Hint.value= "Hint/AttackEnemies"; 
 
 	var inventory= Inventory.GetContext(); 
 	if(GameMode.Parameters.GetBool("OnlyKnives")){ 
-		inventory.Main.Value= false; 
-		inventory.Secondary.Value= false; 
-		inventory.Melee.Value= true; 
-		inventory.Explosive.Value= false; 
-		inventory.Build.Value= true; 
+		inventory.Main.value= false; 
+		inventory.Secondary.value= false; 
+		inventory.Melee.value= true; 
+		inventory.Explosive.value= false; 
+		inventory.Build.value= true; 
 	}else{ 
-		inventory.Main.Value= true; 
-		inventory.Secondary.Value= true; 
-		inventory.Melee.Value= true; 
-		inventory.Explosive.Value= true; 
-		inventory.Build.Value= true; 
+		inventory.Main.value= true; 
+		inventory.Secondary.value= true; 
+		inventory.Melee.value= true; 
+		inventory.Explosive.value= true; 
+		inventory.Build.value= true; 
 	} 
 
-	mainTimer.Restart(GameModeTime); 
+	mainTimer.Restart(GameModeTime); // Игра длится 1 секунду.
 	Spawns.GetContext().Despawn(); 
 	SpawnTeams(); 
 }
+
 function SetEndOfMatch() {
 	scores_timer.Stop(); // выключаем таймер очков
 	const leaderboard= LeaderBoard.GetTeams(); 
+
 	if(leaderboard[0].Weight !== leaderboard[1].Weight){ 
 		SetMockMode(leaderboard[0].Team, leaderboard[1].Team); 
 		
 		for(const win_player of leaderboard[0].Team.Players){ 
 			win_player.Properties.Scores.Value += WINNER_SCORES; 
 		} 
+
 	}else{ 
 		SetEndOfMatch_EndMode(); 
 	} 
+
 }
-function SetMockMode(winners, loosers) { 
-	stateProp.Value= MockModeStateValue; 
-	scores_timer.Stop(); 
 
-	// подсказка
-	Ui.GetContext(winners).Hint.Value= "Hint/MockHintForWinners"; 
-	Ui.GetContext(loosers).Hint.Value= "Hint/MockHintForLoosers"; 
+function SetMockMode(winners, loosers) {  
+	stateProp.value= MockModeStateValue;  
+	scores_timer.Stop();  
 
-	Damage.GetContext().DamageOut.value= true; 
+	// подсказка  
+	Ui.GetContext(winners).Hint.value= "Hint/MockHintForWinners";  
+	Ui.GetContext(loosers).Hint.value= "Hint/MockHintForLoosers";  
 
-	// время спавна для проигравших и победителей
-	var spawnsLosers= Spawns.GetContext(loosers); 
-	spawnsLosers.RespawnTime.value= 2;
+	Damage.GetContext().DamageOut.value= true;  
 
-	var spawnsWinners= Spawns.GetContext(winners); 
-	spawnsWinners.RespawnTime.value= 1;
-
-	var inventoryLosers= Inventory.GetContext(loosers); 
-	inventoryLosers.Main.value= false; 
-
-	var inventoryWinners= Inventory.GetContext(winners); 
-
-	inventoryWinners.MainInfinity.value= true; 
-
-	mainTimer.Restart(MockModeTime); 
+	mainTimer.Restart(MockModeTime);  
 }
-function SetEndOfMatch_EndMode() { 
-	stateProp.value= EndOfMatchStateValue; 
-	scores_timer.Stop(); 
 
-	var spawns= Spawns.getContext(); 
-	spawns.enable=false; 
+function SetEndOfMatch_EndMode() {  
+	stateProp.value= EndOfMatchStateValue;  
+	scores_timer.Stop();  
 
-	Game.GameOver(LeaderBoard.getTeams()); 
+	var spawns= Spawns.getContext();  
+	spawns.enable=false;  
+
+	Game.GameOver(LeaderBoard.getTeams());  
 
 	mainTimer.Restart(EndOfMatchTime);  
 }
@@ -298,6 +293,7 @@ function OnVoteResult(v) {
 	if(v.Result === null) return;  
 	NewGame.RestartGame(v.Result);  
 }  
+
 NewGameVote.OnResult.Add(OnVoteResult);  
 
 function start_vote() {  
