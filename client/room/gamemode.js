1054df48190bd85860054f5790646e6a7dbc8d55
Ygoslavya <michaelnovikovyg95@gmail.com>
2024-12-08 10:10:25 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -3,7 +3,8 @@ import { Game, Players, Inventory, LeaderBoard, BuildBlocksSet, Teams, Damage, B
 import * as teams from './default_teams.js';
 
 // настройки
-const GameDuration = 1; // Игра длится 1 секунда
+const GameDuration = 1;
+const EndOfMatchTime = 1;
 const KILL_SCORES = 5; // Очки за убийство
 const CHEST_SCORES = 10; // Очки за сундук
 
