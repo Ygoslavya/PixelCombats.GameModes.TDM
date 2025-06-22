5a7be2269b68edc399e6db1f64e0c999340c724b
Ygoslavya <michaelnovikovyg95@gmail.com>
2025-06-22 12:59:15 +0300

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -9,10 +9,10 @@ const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
 export function game_mode_length_seconds() {
     const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
     switch (length) {
-        case 'Length_S': return 1; // 4 min
-        case 'Length_M': return 1; // 5 min
-        case 'Length_L': return 1; // 6 min
-        case 'Length_XL': return 1; // 7 min
+        case 'Length_S': return 200; // 4 min
+        case 'Length_M': return 340; // 5 min
+        case 'Length_L': return 320; // 6 min
+        case 'Length_XL': return 380; // 7 min
     }
-    return 1;
+    return 300;
 }
